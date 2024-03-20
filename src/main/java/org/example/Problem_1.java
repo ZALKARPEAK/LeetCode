777aca87992e1716e4f3b2231aa1b37fd3237119
Z <peak@gmail.com>
2024-03-20 14:41:33 +0300

    Initial commit

---
+++ src/main/java/org/example/Problem_1.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Problem_1 {
+    public static int getCommon(int[] a, int[] b) {
+        int i, j;
+        i = 0;
+        j = 0;
+        while (i < a.length && j < b.length) {
+            if (a[i] == b[j])
+                return a[i];
+            else if (a[i] > b[j]) {
+                j++;
+            } else {
+                i++;
+            }
+        }
+        return -1;
+    }
+}
